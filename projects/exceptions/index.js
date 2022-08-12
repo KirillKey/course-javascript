656777db6222c1fffacdf1eba9b0d19201cb5a1c
Kirill Kuklin <keyrealeducation@gmail.com>
2022-08-12 21:19:33 +0300

    HW №3. Misunderstanding of task №3

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -15,9 +15,27 @@
  Пример:
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
- */
-function isAllTrue(array, fn) {}
+   */
+function isAllTrue(array, fn) {
+  if (!(array instanceof Array) || array.length === 0) {
+    throw new Error('empty array');
+  } else if (!(fn instanceof Function)) {
+    throw new Error('fn is not a function');
+  }
 
+  for (const i of array) {
+    if (fn(i) === false) {
+      return false;
+    }
+  }
+  return true;
+}
+// try {
+//   isAllTrue([1, 2, 3, 4, 5], n => n < 10)
+//   isAllTrue([100, 2, 3, 4, 5], n => n < 10)
+// } catch (er) {
+//   console.log(er.message)
+// }
 /*
  Задание 2:
 
@@ -34,8 +52,26 @@ function isAllTrue(array, fn) {}
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array, fn) {
+  if (!(array instanceof Array) || array.length === 0) {
+    throw new Error('empty array');
+  } else if (!(fn instanceof Function)) {
+    throw new Error('fn is not a function');
+  }
 
+  for (const i of array) {
+    if (fn(i) === true) {
+      return true;
+    }
+  }
+  return false;
+}
+// try {
+//   isSomeTrue([1, 2, 30, 4, 5], n => n > 20)
+//   isSomeTrue([1, 2, 3, 4, 5], n => n > 20)
+// } catch (er) {
+//   console.log(er.message)
+// }
 /*
  Задание 3:
 
@@ -47,26 +83,130 @@ function isSomeTrue(array, fn) {}
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn, ...args) {}
 
+////* 1 вариант решения =>
+// function returnBadArguments(fn, ...args) {
+//   const array = [];
+
+//   for (const item of args) {
+//     try {
+//       if (!(fn instanceof Function)) {
+//         throw new Error("fn is not a function")
+//       }
+//       fn(item)
+//     } catch (er) {
+//       console.log(er.message)
+//       array.push(...item)
+//     }
+//   }
+
+//   return array;
+// }
+// returnBadArguments(n => n > 20, [1, 2, 3, 30, 4, 5]) // вернёт []
+// returnBadArguments(n => n > 20, [1, 2, 3, 30, 4, 5]) // вернёт fn is not a function  и  массив (6) [1, 2, 3, 30, 4, 5]  в консоль
+
+//// 2 вариант решения, и по сути, правильный вариант решения, но до конца мне не ясный.
+//// Тестировщик на него не выдаёт ошибок.
+function returnBadArguments(fn, ...args) {
+  const array = [];
+  if (!(fn instanceof Function)) {
+    throw new Error('fn is not a function');
+  }
+
+  for (const item of args) {
+    try {
+      fn(item);
+    } catch (er) {
+      array.push(item);
+    }
+  }
+
+  return array;
+}
+// try {
+//   returnBadArguments(n => n > 20, [1, 2, 3, 30, 4, 5])
+// } catch (er) {
+//   console.log(er.message)
+// }                                        // спокойно вернет в консоли fn is not a function
+
+// returnBadArguments(n => n > 20, [1, 2, 3, 30, 4, 5]) // вернёт []
+// returnBadArguments(20, [1, 2, 3, 30, 4, 5]) // вернёт Uncaught Error: fn is not a function
+
+////////////////////////////////////////////////////////////////////
+
+//// 3 вариант решения =>
+// function returnBadArguments(fn, ...args) {
+//   const array = [];
+
+//   for (const item of args) {
+//     try {
+//       if (!(fn instanceof Function)) {
+//         throw new Error("fn is not a function")
+//       }
+//       fn(item)
+//     } catch (er) {
+//       array.push(item)
+//     }
+//   }
+
+//   return array;
+// }
+// returnBadArguments(n => n > 20, [1, 2, 3, 30, 4, 5]) // вернёт []
+// returnBadArguments(20, [1, 2, 3, 30, 4, 5]) // вернёт [Array(6)]
 /*
- Задание 4:
+Задание 4:
+ 
+4.1: Функция имеет параметр number (по умолчанию - 0)
 
- 4.1: Функция имеет параметр number (по умолчанию - 0)
+4.2: Функция должна вернуть объект, у которого должно быть несколько методов:
+ - sum - складывает number с переданными аргументами
+ - dif - вычитает из number переданные аргументы
+ - div - делит number на первый аргумент. Результат делится на следующий аргумент (если передан) и так далее
+ - mul - умножает number на первый аргумент. Результат умножается на следующий аргумент (если передан) и так далее
 
- 4.2: Функция должна вернуть объект, у которого должно быть несколько методов:
-   - sum - складывает number с переданными аргументами
-   - dif - вычитает из number переданные аргументы
-   - div - делит number на первый аргумент. Результат делится на следующий аргумент (если передан) и так далее
-   - mul - умножает number на первый аргумент. Результат умножается на следующий аргумент (если передан) и так далее
+Количество передаваемых в методы аргументов заранее неизвестно
 
- Количество передаваемых в методы аргументов заранее неизвестно
+4.3: Необходимо выбрасывать исключение в случаях:
+ - number не является числом (с текстом "number is not a number")
+ - какой-либо из аргументов div является нулем (с текстом "division by 0")
+*/
+function calculator(number = 0) {
+  if (typeof number !== 'number') {
+    throw new Error('number is not a number');
+  }
 
- 4.3: Необходимо выбрасывать исключение в случаях:
-   - number не является числом (с текстом "number is not a number")
-   - какой-либо из аргументов div является нулем (с текстом "division by 0")
- */
-function calculator(number = 0) {}
+  return {
+    sum(...args) {
+      for (const arg of args) {
+        return (number += arg);
+      }
+    },
+    dif(...args) {
+      for (const arg of args) {
+        return (number -= arg);
+      }
+    },
+    div(...args) {
+      for (const arg of args) {
+        if (arg === 0) {
+          throw new Error('division by 0');
+        }
+        return (number /= arg);
+      }
+    },
+    mul(...args) {
+      for (const arg of args) {
+        return (number *= arg);
+      }
+    },
+  };
+}
+
+// try {
+//   calculator(2).sum(2)
+// } catch (er) {
+//   console.log(er.message)
+// }
 
 /* При решении задач, постарайтесь использовать отладчик */
 
